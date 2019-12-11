849e753acc65d503a451a04657fa027b0cbe5712
Kishore <kishore@prestomall.com>
2019-12-12 00:17:05 +0800

    > Feature four

--- TestRepo/TestRepo/ViewController.swift
+++ TestRepo/TestRepo/ViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        featureFour()
         // Do any additional setup after loading the view.
+    }
+    func featureFour()  {
+        print("feature Four")
     }
 
 
